6e8979cff100179c00e992308f158bef4206b488
jsdysw <jsdysw@gmail.com>
2022-04-30 00:14:35 +0900

    handling multi client is done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # socket-programming
+
+## simeple TCP, UDP socket programming
+
+the tcp server can handle only one client connection 
+
+## Multi-Client TCP socket programming
--- multi-client-tcp/MultiClientTCPServer.go
+++ multi-client-tcp/MultiClientTCPServer.go
@@ -1,5 +1,5 @@
 /**
- * EasyTCPServer.go
+ * MultiClientTCPServer.go
  * NAME : Seokwon Yoon
  * ID : 20174089
  **/
@@ -18,76 +18,43 @@ import (
     "syscall"
 )
 
- 
-func main() {
-    time_start := time.Now()
+func handleConnection(client_conn net.Conn, id int) {
+    // if main go routine ends, close all the client conns
+    // c := make(chan os.Signal)
+    // signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    // go func() {
+    //     <-c
+    //     Listener.Close()
+    //     // 열려있는 애들 다 닫자.
+    //     fmt.Println("--------client closed")
+    //     os.Exit(0)
+    // }()
 
-    total_served_commands := 0
-    serverPort := "44089"
- 
-    listener, err:= net.Listen("tcp", ":" + serverPort)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
-
-    var client_conn net.Conn
-
-    // ctrl + c handling
-    c := make(chan os.Signal)
-    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-    go func() {
-        <-c
-        listener.Close()
-        fmt.Println("Bye bye~")
-        os.Exit(0)
-        return
-    }()
-
-    conn, err:= listener.Accept()
-    if err != nil {
-        log.Fatal(err)
-    } else {
-        client_conn = conn
-        fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-    }
-    
     for {
-        // ctrl + c handling
-        c := make(chan os.Signal)
-        signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-        go func() {
-            <-c
-            client_conn.Close()
-            listener.Close()
-            fmt.Println("Bye bye~")
-            os.Exit(0)
-            return
-        }()
-        
         buffer := make([]byte, 1024)
-            
+
         count, err := client_conn.Read(buffer)
         if err != nil {
+            fmt.Printf("client socket failed to read data from buffer\n")
             log.Fatal(err)
         }
         // check the header of the message
         fmt.Printf("Command %s\n",string(buffer[0]))
         switch string(buffer[0]) {
         case "1": 
-            total_served_commands += 1
+            Total_served_commands += 1
             client_conn.Write(bytes.ToUpper(buffer[1:count]))
         case "2":
-            total_served_commands += 1
+            Total_served_commands += 1
             client_conn.Write([]byte(client_conn.RemoteAddr().String()))   
         case "3":
-            total_served_commands += 1
-            served_count_string := strconv.Itoa(total_served_commands)
+            Total_served_commands += 1
+            served_count_string := strconv.Itoa(Total_served_commands)
             client_conn.Write([]byte(served_count_string)) 
         case "4":
-            total_served_commands += 1
-            time_elapsed := time.Since(time_start)
+            Total_served_commands += 1
+
+            time_elapsed := time.Since(Time_start)
 
             // result should be HH:MM:SS format
             hh_s := strconv.Itoa(int(time_elapsed.Hours()))
@@ -106,19 +73,60 @@ func main() {
 
             client_conn.Write([]byte(result))
         case "5":
-            // close client conn and wait anotherå
+            // close client connection
+            TotalClient = TotalClient - 1;
             client_conn.Close()
-            // fmt.Printf("waiting for another client\n")
-            conn, err:= listener.Accept()
-            if err != nil {
-                log.Fatal(err)
-            } else {
-                client_conn = conn
-                fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-            }
+            fmt.Printf("Client %d disconnected. Number of connected clients = %d\n", id, TotalClient)
+            return
         default :
             fmt.Printf("Wrong option\n")
         }
+    }
+    // close client conn 
+}
+
+var Total_served_commands = 0
+var LastestId = 0
+var TotalClient = 0
+var Time_start = time.Now()
+var Listener net.Conn
+
+func main() {
+    serverPort := "44089"
+ 
+    // create server socket
+    Listener, err:= net.Listen("tcp", ":" + serverPort)
+    if err != nil {
+        fmt.Printf("create server socket failed\n")
+        log.Fatal(err)
+    }
+    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+
+    // ctrl + c handling
+    c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        <-c
+        Listener.Close()
+        fmt.Println("Bye bye~")
+        os.Exit(0)
+    }()
+
+    for {
+        // create client socket
+        conn, err:= Listener.Accept()
+        if err != nil {
+            // fmt.Printf("create client socket failed\n")
+            log.Fatal(err)
+        } else {
+            TotalClient = TotalClient + 1;
+            LastestId = LastestId + 1;
+            fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
+            fmt.Printf("Client %d connected. Number of connected clients = %d\n", LastestId, TotalClient)
+        }
+        
+        // client socket do works
+        go handleConnection(conn, LastestId)
     }
 }
  
